8b97db17fc794fce67e428dca710215c9fe097ec
Trevor Davis <tzendou@gmail.com>
2019-02-17 22:00:09 -0800

    Add unit test for save method

---
+++ src/test/java/com/trevor/deliveryservice/service/ContactsManagementServiceUnitTest.java
@@ -0,0 +1,50 @@
+package com.trevor.deliveryservice.service;
+
+import com.trevor.deliveryservice.data.repository.CustomerContactRepository;
+import com.trevor.deliveryservice.domain.CustomerContact;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public class ContactsManagementServiceUnitTest {
+
+    @Mock
+    private CustomerContactRepository customerContactRepository;
+
+    @InjectMocks
+    private ContactsManagementService contactsManagementService;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testSaveContactHappyPath() {
+
+        // Create a contact
+        CustomerContact mockContact = new CustomerContact();
+        mockContact.setFirstName("Jenny");
+        mockContact.setLastName("Smith");
+
+        when(customerContactRepository.save(any(CustomerContact.class))).thenReturn(mockContact);
+
+        // Save the contact
+        CustomerContact newContact = contactsManagementService.save(mockContact);
+
+        // Verify the save
+        verify(customerContactRepository, atLeastOnce()).save(mockContact);
+        assertEquals("Jenny", newContact.getFirstName());
+    }
+}
