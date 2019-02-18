aab97fb7ebd2d6af636a89e5dfe4f4a47ced572e
Trevor Davis <tzendou@gmail.com>
2019-02-17 20:44:03 -0800

    update models with id to reference type 'Long'

--- src/main/java/com/trevor/deliveryservice/domain/CustomerContact.java
+++ src/main/java/com/trevor/deliveryservice/domain/CustomerContact.java
@@ -8,7 +8,7 @@ public class CustomerContact {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    private Long id;
     private String firstName;
     private String lastName;
     private String email;
@@ -18,11 +18,11 @@ public class CustomerContact {
     private String deliveryAddressState;
     private String deliveryAddressLZipCode;
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
--- src/main/java/com/trevor/deliveryservice/domain/DeliveryAddress.java
+++ src/main/java/com/trevor/deliveryservice/domain/DeliveryAddress.java
@@ -8,18 +8,18 @@ public class DeliveryAddress {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    private Long id;
     private String line1;
     private String line2;
     private String city;
     private String state;
     private String zipCode;
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
