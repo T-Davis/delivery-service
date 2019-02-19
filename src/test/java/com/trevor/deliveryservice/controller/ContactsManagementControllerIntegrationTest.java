85f070aad13ee33305a3ae23f3c5a5db182e51d2
Trevor Davis <tzendou@gmail.com>
2019-02-18 21:00:47 -0800

    Add test for /addContact POST method

---
+++ src/test/java/com/trevor/deliveryservice/controller/ContactsManagementControllerIntegrationTest.java
@@ -0,0 +1,34 @@
+package com.trevor.deliveryservice.controller;
+
+import com.trevor.deliveryservice.domain.CustomerContact;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ContactsManagementControllerIntegrationTest {
+
+    @Autowired
+    private ContactsManagementController contactsManagementController;
+
+    @Test
+    public void testAddContactHappyPath() {
+
+        CustomerContact contact = new CustomerContact();
+        contact.setFirstName("Jenny");
+        contact.setLastName("Johnson");
+
+        // POST CustomerContact
+        String outcome = contactsManagementController.processContactSubmit(contact);
+
+        // Verify the outcome
+        assertThat(outcome, is(equalTo("Success")));
+    }
+}
